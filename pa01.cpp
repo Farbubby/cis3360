48c59acdc9038ee7edf0d94a6f5ad879ba2ff54e
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-06-09 19:52:56 -0400

    reading from a plaintext file

--- pa01.cpp
+++ pa01.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main() {
-    cout << "Hello World!" << endl;
+int main(int args, char *argv[]) {
+
+    if (args != 3)
+    {
+        return 0;
+    }
+
+    string key {argv[1]};
+    //string text {argv[2]};
+    string output;
+    string buffer;
+
+    ifstream keyFile {key};
+    ifstream textFile {};
+
+    //textStream.open(text);
+
+    if (!keyFile.is_open()) return 0;
+
+    while (!keyFile.eof())
+    {
+        keyFile >> buffer;
+        if (keyFile.eof()) break;
+        output += buffer;
+    }
+
+    cout << output << endl;
+
+    keyFile.close();
+
+    return 0;
 }
