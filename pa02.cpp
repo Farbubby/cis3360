08b53d38dd0118d68af81bd8698f796bc9b63734
farbubby <farhanmahbub88@Knights.ucf.edu>
2023-07-11 16:33:06 -0400

    reads the input text and validates checksum size

--- pa02.cpp
+++ pa02.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 using namespace std;
 
+string readText(string filePath);
+// string checkSum8(string text);
+// string checkSum16(string text);
+// string checkSum32(string text);
+
 int main(int args, char *argv[]) {
-    return 0;
+    
+    if (args != 3) return 0;
+
+    string text = readText(argv[1]);
+
+    if (stoi(argv[2]) == 8) {
+        //checkSum8(text);
+        return 0;
+    }
+    else if (stoi(argv[2]) == 16)
+        //checkSum16(text);
+        return 1;
+    else if (stoi(argv[2]) == 32)
+        //checkSum32(text);
+        return 1;
+    else
+        cerr << "Invalid checksum type" << endl;
+}
+
+string readText(string filePath) {
+
+    string text;
+    ifstream inputFile;
+
+    inputFile.open(filePath);
+
+    if (!inputFile.is_open()) return "";
+
+    while (!inputFile.eof()) {
+        string buffer;
+        getline(inputFile, buffer);
+        text += buffer;
+    }
+
+    text += "\n";
+
+    return text;
 }
